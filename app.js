436012ab4a1130dfe5c531ec03aea5ec8cdb5bbe
Steven <125781365+stevenfarless@users.noreply.github.com>
2025-12-01 06:44:41 -0600

    fix abbreviations in modal book selector

--- app.js
+++ app.js
@@ -587,27 +587,50 @@ class BibleApp {
     }
 
     populateBookModal() {
-        const createBookButton = (book) => {
-            const btn = document.createElement('button');
-            btn.className = 'book-item';
-            btn.textContent = book;
-            btn.addEventListener('click', () => {
-                this.loadPassage(book, 1);
-                this.closeModal(this.bookModal);
-            });
-            return btn;
-        };
+    const abbreviations = {
+        'Genesis': 'Gen', 'Exodus': 'Exod', 'Leviticus': 'Lev', 'Numbers': 'Num', 'Deuteronomy': 'Deut',
+        'Joshua': 'Josh', 'Judges': 'Judg', 'Ruth': 'Ruth', '1 Samuel': '1 Sam', '2 Samuel': '2 Sam',
+        '1 Kings': '1 Kgs', '2 Kings': '2 Kgs', '1 Chronicles': '1 Chr', '2 Chronicles': '2 Chr',
+        'Ezra': 'Ezra', 'Nehemiah': 'Neh', 'Esther': 'Esth', 'Job': 'Job', 'Psalms': 'Ps',
+        'Proverbs': 'Prov', 'Ecclesiastes': 'Eccl', 'Song of Solomon': 'Song', 'Isaiah': 'Isa',
+        'Jeremiah': 'Jer', 'Lamentations': 'Lam', 'Ezekiel': 'Ezek', 'Daniel': 'Dan',
+        'Hosea': 'Hos', 'Joel': 'Joel', 'Amos': 'Amos', 'Obadiah': 'Obad', 'Jonah': 'Jonah',
+        'Micah': 'Mic', 'Nahum': 'Nah', 'Habakkuk': 'Hab', 'Zephaniah': 'Zeph',
+        'Haggai': 'Hag', 'Zechariah': 'Zech', 'Malachi': 'Mal',
 
-        this.oldTestamentBooks.innerHTML = '';
-        Object.keys(this.bibleBooks['Old Testament']).forEach(book => {
-            this.oldTestamentBooks.appendChild(createBookButton(book));
-        });
+        'Matthew': 'Matt', 'Mark': 'Mark', 'Luke': 'Luke', 'John': 'John', 'Acts': 'Acts',
+        'Romans': 'Rom', '1 Corinthians': '1 Cor', '2 Corinthians': '2 Cor', 'Galatians': 'Gal',
+        'Ephesians': 'Eph', 'Philippians': 'Phil', 'Colossians': 'Col',
+        '1 Thessalonians': '1 Thess', '2 Thessalonians': '2 Thess',
+        '1 Timothy': '1 Tim', '2 Timothy': '2 Tim', 'Titus': 'Titus',
+        'Philemon': 'Phlm', 'Hebrews': 'Heb', 'James': 'Jas',
+        '1 Peter': '1 Pet', '2 Peter': '2 Pet',
+        '1 John': '1 John', '2 John': '2 John', '3 John': '3 John',
+        'Jude': 'Jude', 'Revelation': 'Rev'
+    };
 
-        this.newTestamentBooks.innerHTML = '';
-        Object.keys(this.bibleBooks['New Testament']).forEach(book => {
-            this.newTestamentBooks.appendChild(createBookButton(book));
+    const createBookButton = (book) => {
+        const btn = document.createElement('button');
+        btn.className = 'book-item';
+        btn.textContent = abbreviations[book] || book; // display abbreviation only
+        btn.addEventListener('click', () => {
+            this.loadPassage(book, 1);                  // still use full name
+            this.closeModal(this.bookModal);
         });
-    }
+        return btn;
+    };
+
+    this.oldTestamentBooks.innerHTML = '';
+    Object.keys(this.bibleBooks['Old Testament']).forEach(book => {
+        this.oldTestamentBooks.appendChild(createBookButton(book));
+    });
+
+    this.newTestamentBooks.innerHTML = '';
+    Object.keys(this.bibleBooks['New Testament']).forEach(book => {
+        this.newTestamentBooks.appendChild(createBookButton(book));
+    });
+}
+
 
     openChapterModal() {
         this.populateChapterModal();
